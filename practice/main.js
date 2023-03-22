65097159dfd17745f95ba3b74024d250cc3c8bd3
iyong-il <lyi1233313@gmail.com>
2023-03-22 20:45:39 +0900

    23.03.22 / 퍼블리싱 연습

--- practice/main.js
+++ practice/main.js
@@ -1,33 +1,26 @@
-// progressbar.js@1.0.0 version is used
-// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+var chi = document.getElementById("merong");
+var chet = chi.getContext("2d");
 
-var bar = new ProgressBar.SemiCircle(container, {
-  strokeWidth: 5.75,
-  easing: "easeInOut",
-  duration: 1400,
-  color: "#FF5000",
-  trailColor: "#ddd",
-  trailWidth: 5.75,
-  svgStyle: null,
-});
+chet.beginPath();
+chet.strokeStyle = "#ff5000";
+chet.lineWidth = 8;
+chet.arc(170, 200, 152, 0, Math.PI, true);
+chet.stroke();
 
-var bar2 = new ProgressBar.SemiCircle(container2, {
-  strokeWidth: 8,
-  easing: "easeInOut",
-  duration: 1400,
-  color: "#00a7e1",
-  trailColor: "#ddd",
-  trailWidth: 8,
-  svgStyle: null,
-});
-bar.animate(0.9);
-bar2.animate(0.5); // Number from 0.0 to 1.0
+chet.beginPath();
+chet.strokeStyle = "#ddd";
+chet.lineWidth = 8;
+chet.arc(170, 200, 152, 0, -0.5 * Math.PI, true);
+chet.stroke();
 
-function checkInputLength() {
-  var input = document.getElementById("myInput");
-  if (input.value.length >= 13) {
-    input.classList.add("border");
-  } else {
-    input.classList.remove("border");
-  }
-}
+chet.beginPath();
+chet.strokeStyle = "#00a7e1";
+chet.lineWidth = 8;
+chet.arc(170, 200, 140, 0, Math.PI, true);
+chet.stroke();
+
+chet.beginPath();
+chet.strokeStyle = "#ddd";
+chet.lineWidth = 8;
+chet.arc(170, 200, 140, 0, -0.4 * Math.PI, true);
+chet.stroke();
