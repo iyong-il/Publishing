a69721f736c0946eaebf5d560de282ec6eb37aa2
iyong-il <lyi1233313@gmail.com>
2023-03-26 16:27:52 +0900

    23.03.26 / css 연습

--- practice/main.js
+++ practice/main.js
@@ -1,26 +1,6 @@
-var chi = document.getElementById("merong");
-var chet = chi.getContext("2d");
+const progressBar = document.querySelector(".progress-bar__value");
+const animateProgressBar = (progress) => {
+  progressBar.style.transform = `scaleY(${progress}) translateX(-50%)`;
+};
 
-chet.beginPath();
-chet.strokeStyle = "#ff5000";
-chet.lineWidth = 8;
-chet.arc(170, 200, 152, 0, Math.PI, true);
-chet.stroke();
-
-chet.beginPath();
-chet.strokeStyle = "#ddd";
-chet.lineWidth = 8;
-chet.arc(170, 200, 152, 0, -0.5 * Math.PI, true);
-chet.stroke();
-
-chet.beginPath();
-chet.strokeStyle = "#00a7e1";
-chet.lineWidth = 8;
-chet.arc(170, 200, 140, 0, Math.PI, true);
-chet.stroke();
-
-chet.beginPath();
-chet.strokeStyle = "#ddd";
-chet.lineWidth = 8;
-chet.arc(170, 200, 140, -0, -0.4 * Math.PI, true);
-chet.stroke();
+animateProgressBar(0.4); // 프로그레스 바 시작
